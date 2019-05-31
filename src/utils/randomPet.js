d45c43927869dd2672fedb5180be1c991224ab46
Samantha Dickeson <samlucydickeson@gmail.com>
2019-05-31 11:17:30 +0100

    Made randomPet return a Promise Fixes #9 Fixes #9

--- src/utils/randomPet.js
+++ src/utils/randomPet.js
@@ -1,15 +1,18 @@
-import {getAllPets} from './getPetData';
+import { getAllPets } from "./getPetData";
 
-const randomPet = (func) => {
-    return getAllPets().then(response => {
-        const allNames = response.map(pet => pet.name);
-        const max = allNames.length;
-        const random = Math.floor(Math.random() * max);
-        console.log(allNames[random]);
-        func(allNames[random])
-
+const randomPet = () => {
+  return getAllPets()
+    .then(response => {
+      const allNames = response.map(pet => pet.name);
+      const max = allNames.length;
+      const random = Math.floor(Math.random() * max);
+      console.log(allNames[random]);
+      return new Promise((resolve, reject) => {
+        resolve(allNames[random]);
+        reject("sorry, nope");
+      });
     })
-    .catch(err => console.log(err))
+    .catch(err => console.log(err));
 };
 
 export default randomPet;
